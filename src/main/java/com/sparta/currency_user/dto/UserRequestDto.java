fa64cd94755cea3eac426be18f503a6961413749
조현지 <qndjddl001@gmail.com>
2024-11-29 09:45:46 +0900

    fix: 예외처리 수정

--- src/main/java/com/sparta/currency_user/dto/UserRequestDto.java
+++ src/main/java/com/sparta/currency_user/dto/UserRequestDto.java
@@ -2,16 +2,16 @@ package com.sparta.currency_user.dto;
 
 import com.sparta.currency_user.entity.User;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserRequestDto {
-    @NotNull(message="이름을 입력해 주세요.")
+    @NotBlank(message="이름을 입력해 주세요.")
     private String name;
 
     @Email(message="이메일 형식을 확인해 주세요.")
-    @NotNull(message="이메일을 입력해 주세요.")
+    @NotBlank(message="이메일을 입력해 주세요.")
     private String email;
 
     public User toEntity() {
